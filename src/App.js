6bcba4986c814da54f8c730c1b1465cf50b22a32
Satyam Singh <satyam.12011172@gmail.com>
2023-10-23 18:51:11 +0530

    initialised

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Logo from './images/Logo-png.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Logo}  alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Visitor
+        </p>
+        <p>
+          We Are Building!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/andrew-lee-0a2a2a1b0/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
+      
     </div>
   );
 }
